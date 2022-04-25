4e433cfcb46cd001ebde50355c51bccbaaa5d5d6
doaamagdy2024 <doaa.magdy2001@gmail.com>
2022-04-25 05:57:55 +0200

    --

--- scheduler.c
+++ scheduler.c
@@ -5,22 +5,22 @@
 process* Process_Table;
 
 
-    void Context_Switching_To_Run(int Entry_Number)
-    {
-        int Process_id = Process_Table[Entry_Number].id;
-        Process_Table[Entry_Number].state=0;
-        Process_Table[Entry_Number].waitingtime +=(getClk()-(Process_Table[Entry_Number].waiting_start_time));
-        Process_Table[Entry_Number].running_start_time=getClk(); 
-        kill(Process_id,SIGCONT); //continue the stopped process
-    }
-    void Context_Switching_To_Wait(int Entry_Number)
-    {
-        int Process_id = Process_Table[Entry_Number].id;
-        Process_Table[Entry_Number].state=1;
-        Process_Table[Entry_Number].remainingtime-=(getClk()-(Process_Table[Entry_Number].running_start_time));
-        Process_Table[Entry_Number].waiting_start_time=getClk();
-        kill(Process_id,SIGSTOP); //stopping it to the waiting state
-    }
+void Context_Switching_To_Run(int Entry_Number)
+{
+    int Process_id = Process_Table[Entry_Number].id;
+    Process_Table[Entry_Number].state=0;
+    Process_Table[Entry_Number].waitingtime +=(getClk()-(Process_Table[Entry_Number].waiting_start_time));
+    Process_Table[Entry_Number].running_start_time=getClk(); 
+    kill(Process_id,SIGCONT); //continue the stopped process
+}
+void Context_Switching_To_Wait(int Entry_Number)
+{
+    int Process_id = Process_Table[Entry_Number].id;
+    Process_Table[Entry_Number].state=1;
+    Process_Table[Entry_Number].remainingtime-=(getClk()-(Process_Table[Entry_Number].running_start_time));
+    Process_Table[Entry_Number].waiting_start_time=getClk();
+    kill(Process_id,SIGSTOP); //stopping it to the waiting state
+}
 
 
 
