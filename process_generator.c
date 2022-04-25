3cb150ad6c9a7331f70cec7e2065d582c10a92a4
Rufaida-Kassem <rufaidahkassem@gmail.com>
2022-04-25 02:55:16 +0200

    input

--- process_generator.c
+++ process_generator.c
@@ -1,4 +1,6 @@
 #include "headers.h"
+#include <string.h>
+#define LINE_SIZE 300
 
 void clearResources(int);
 
@@ -7,6 +9,24 @@ int main(int argc, char * argv[])
     signal(SIGINT, clearResources);
     // TODO Initialization
     // 1. Read the input files.
+    int process[4];
+    int i;
+    FILE * pFile;
+    char* line = malloc(LINE_SIZE);
+    int parameter;
+
+    pFile = fopen("processes.txt", "r");
+    while(fgets(line, LINE_SIZE, pFile) != NULL){
+        
+        if(line[0] == '#'){continue;}
+        process[0] = strtol(strtok(line, "\t"), NULL, 10);
+        for (i = 1; i < 4; i++)
+            process[i] = atoi(strtok(NULL, "\t"));
+        for (i = 0; i < 4; i++)
+            printf("%d\t", process[i]);
+        printf("\n");
+    }
+
     // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
     // 3. Initiate and create the scheduler and clock processes.
     // 4. Use this function after creating the clock process to initialize clock
